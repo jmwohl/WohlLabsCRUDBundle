de9a21bf3aae9c0be6d559350404f2d9a657a804
Jonathan Wohl <jon@jonwohl.com>
2011-09-17 09:22:40 -0500

    initial commit - skeleton templates extend base, routing manipulator fixed

---
+++ Bundle/CRUDBundle/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace WohlLabs\Bundle\CRUDBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DefaultController extends Controller
+{
+    
+    public function indexAction($name)
+    {
+        return $this->render('WohlLabsCRUDBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ Bundle/CRUDBundle/WohlLabsCRUDBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WohlLabs\Bundle\CRUDBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WohlLabsCRUDBundle extends Bundle
+{
+}
